1c40f0e0e92f504945701f9c72ac05a52e483825
Vinod <Vinodkumarabbaigari@gmail.com>
2025-03-28 14:57:43 +0530

    This is my change

--- First.java
+++ First.java
@@ -2,5 +2,8 @@ class First{
 public static void main(String args[]){
 System.out.println("Hello World");
 System.out.print("Hello vinod kumar kambayagari");
+int a =10;
+int b=20;
+System.out.println(a+b);
 }
 }
